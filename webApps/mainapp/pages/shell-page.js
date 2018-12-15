24e7513a76cbf10dfb07209354ade57ff82bf6ce
Andrejus <abaranovskis@redsamuraiconsulting.com>
2018-12-15 17:47:27 +0200

    Added support for menu slider

--- webApps/mainapp/pages/shell-page.js
+++ webApps/mainapp/pages/shell-page.js
@@ -1,7 +1,19 @@
-define([], function() {
+define(['ojs/ojcore'], function(oj) {
   'use strict';
 
   var PageModule = function PageModule() {};
+  
+  PageModule.prototype.showSide = function() {
+    var offcanvas = {
+      "selector": "#startDrawer",
+      "content": "#page",
+      "edge": "start",
+      "displayMode": "push",
+      "size": "300px"
+    };
+ 
+    oj.OffcanvasUtils.open(offcanvas);
+  }
 
   return PageModule;
 });
